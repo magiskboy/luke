839dcd327b63f24b895b0548695c47a72ec8ae5c
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2023-07-02 16:31:41 +0700

    chore: prepare release 0.0.1

--- luke/__init__.py
+++ luke/__init__.py
@@ -2,4 +2,4 @@ from .generators import ObjectFieldGenerator   # type: ignore # noqa
 from .server import ServerGenerator       # type: ignore # noqa
 from .openapi import OpenAPISpec                    # type: ignore # noqa
 
-__version__ = '0.0.1-dev1'
+__version__ = '0.0.1'
